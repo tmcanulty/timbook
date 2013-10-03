76fe912f01fdce06377b930f90429ada0b622155
Tim McAnulty <tmcanulty@gmail.com>
2013-10-03 14:49:18 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timbook::Application.config.secret_token = '3a1ea3c4e15364d89ab31e003b5c442355a74bb650efc6058aa156aa8028b683ad15e92d008e0a0592d6295630561d1bdeb4586dd1da4e0f11587a74055c7f6e'
